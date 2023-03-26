658b8c79bb19db6451911175c4f1f07aa89969b5
Tom <tom.slaytor@gmail.com>
2023-03-26 18:57:31 +0300

    switching to composer to try to get PHPUnit to work

---
+++ Tests/dateTest.php
@@ -0,0 +1,13 @@
+<?php
+
+require 'phpunit-10.0.17.phar';
+// require_once 'Functions/Month.php';
+
+class dateTest extends \PHPUnit\Framework\TestCase
+{
+    public function testDifferentMonths()
+    {
+        // Month::same('2023-01-31', '2023-01-01')
+        // $this->assertEquals(Month::same('2023-01-31', '2023-01-01'), true);
+    }
+}
