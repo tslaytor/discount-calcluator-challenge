3f5bd16292a4b9f1c34b355ca70ebaee34f1665b
Tom <tom.slaytor@gmail.com>
2023-03-26 20:44:04 +0300

    fixed some file path issues, and wrote date function tests

--- Tests/dateTest.php
+++ Tests/dateTest.php
@@ -2,12 +2,35 @@
 
 require_once __DIR__ . '/../code/Functions/Month.php';
 
-class dateTest extends \PHPUnit\Framework\TestCase
+use \PHPUnit\Framework\TestCase;
+
+class dateTest extends TestCase
 {
-    public function testSameMonths()
+    public function testSameMonthsTrue()
     {
         $date1 = '2023-01-31';
         $date2 = '2023-01-01';
         $this->assertTrue(Month::same($date1, $date2));
+    }
+
+    public function testSameMonthsFalse()
+    {
+        $date1 = '2023-01-31';
+        $date2 = '2023-02-01';
+        $this->assertFalse(Month::same($date1, $date2));
+    }
+
+    public function testDifferentMonthsTrue()
+    {
+        $date1 = '2023-01-31';
+        $date2 = '2023-02-01';
+        $this->assertTrue(Month::different($date1, $date2));
+    }
+
+    public function testDifferentMonthsFalse()
+    {
+        $date1 = '2023-01-31';
+        $date2 = '2023-01-01';
+        $this->assertFalse(Month::different($date1, $date2));
     }
 }
---
+++ Tests/fileParserTest.php
@@ -0,0 +1,12 @@
+<?php
+
+use \PHPUnit\Framework\TestCase;
+
+class fileParserTest extends TestCase
+{
+    public function testExample()
+    {
+        
+        $this->assertEquals(1, 1);
+    }
+}
